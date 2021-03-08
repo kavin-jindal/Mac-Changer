2e849ca4bdd992780293d2747a9eedfeec0a2ba8
CyDoc.py <68228966+kavinjindal@users.noreply.github.com>
2021-03-08 17:41:31 +0530

    Create mac2.py

---
+++ Python2/mac2.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+import time
+print("Mac Changer")
+subprocess.call('ifconfig')
+print("Mac Changer for Python2")
+
+def mac():
+    try:
+        int_name = raw_input("Enter interface name:")
+        add_name = raw_input('Enter new Address : ')
+        subprocess.call(['ifconfig ' + int_name + ' down'], shell=True)
+        subprocess.call(['ifconfig ' + int_name + ' hw ' + 'ether ' +  add_name], shell=True)
+        subprocess.call(['ifconfig ' +  int_name +  ' up'], shell=True)
+        #subprocess.call(['ifconfig ' +  int_name +  ' ether'], shell=True)
+        print("Your mac address was changed successfully!!!!!")
+        time.sleep(2)
+        print('Your new mac address' + add_name)
+        subprocess.call(['ifconfig'])
+
+    except:
+        time.sleep(3)
+        print('===================================')
+        print('An error occurred, make sure your interface name or new mac address was correct, try running the script again..')
+
+
+mac()
