ba76200559c121db63008ed6d065cb1563466ec6
CyDoc.py <68228966+kavinjindal@users.noreply.github.com>
2021-03-22 15:32:57 +0530

    Update mac3.py

--- Python3/mac3.py
+++ Python3/mac3.py
@@ -1,27 +1,37 @@
 import subprocess
 import os
 import time
-print("Mac Changer")
-subprocess.call('ifconfig')
-print("Mac Changer for Python3")
+import platform
+from colorama import *
+print(Fore.CYAN + "Mac Changer")
+print(Fore.CYAN + "Mac Changer for Python3")
+print(Fore.GREEN + '=================================')
+print(Fore.YELLOW + f'User System : {platform.system()}')
+print(Fore.YELLOW + f'OS Release  : {platform.release()}')
+print(Fore.YELLOW + f'Machine : {platform.machine()}')
+print(Fore.YELLOW + f'Python Version : {platform.python_version()}')
+print(Fore.GREEN + '=================================')
+input("Press to continue >> ")
+
 
 def mac():
     try:
+        subprocess.call(['ifconfig'])
         int_name = input("Enter interface name:")
         add_name = input('Enter new Address : ')
         subprocess.call(['ifconfig ' + int_name + ' down'], shell=True)
         subprocess.call(['ifconfig ' + int_name + ' hw ' + 'ether ' +  add_name], shell=True)
         subprocess.call(['ifconfig ' +  int_name +  ' up'], shell=True)
         #subprocess.call(['ifconfig ' +  int_name +  ' ether'], shell=True)
-        print("Your mac address was changed successfully!!!!!")
+        print(Fore.GREEN + "Your mac address was changed successfully!!!!!")
         time.sleep(2)
-        print('Your new mac address' + add_name)
+        print(Fore.GREEN + 'Your new mac address' + add_name)
         subprocess.call(['ifconfig'])
 
     except:
         time.sleep(3)
-        print('===================================')
-        print('An error occurred, make sure your interface name or new mac address was correct, try running the script again..')
+        print(Fore.RED + '===================================')
+        print(Fore.RED + 'An error occurred, make sure your interface name or new mac address was correct, try running the script again..')
 
 
 mac()
